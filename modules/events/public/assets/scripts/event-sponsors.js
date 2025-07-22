8e9e889425ad520d7497740b903e2b201663cb77
ivory <ivory@combinate.me>
2025-07-22 15:20:18 +0800

    [TW#24872466] Sponsors

---
+++ modules/events/public/assets/scripts/event-sponsors.js
@@ -0,0 +1,40 @@
+const sponsorsDataEl = document.getElementById('sponsors-data');
+const sponsorItems = document.querySelectorAll('.event-sponsors .cell');
+const sponsorModal = document.getElementById('sponsor-modal');
+const sponsorContent = document.getElementById('sponsor-content');
+
+if (sponsorsDataEl) {
+     const mapData = JSON.parse(sponsorsDataEl.textContent);
+     const { sponsors } = mapData;
+
+     sponsorItems.forEach(sponsorItem => {
+          sponsorItem.addEventListener('click', () => {
+               const sponsor = sponsors.find(item => item.id === sponsorItem.dataset.id);
+               if (!sponsor) return;
+
+               sponsorModal.heading = sponsor.company_name;
+               sponsorContent.innerHTML = sponsor.content;
+
+               // Remove any existing sub-heading
+               const oldSubHeading = document.querySelector('#sponsor-modal .sub-heading');
+               if (oldSubHeading) oldSubHeading.remove();
+
+               const subHeadingHtml = `
+               <div class="sub-heading">
+                    <div class="spacer x-small"></div>
+                    <div class="event-card-wrap">
+                         <div class="grid-x">
+                         <i class="icon-globals"></i>
+                         <span id="sponsor-email">${sponsor.link}</span>
+                         </div>
+                    </div>
+                    <div class="spacer"></div>
+                    <hr>
+               </div>
+               `;
+
+               document.querySelector('#sponsor-modal .icon-close-1').insertAdjacentHTML('afterend', subHeadingHtml);
+               sponsorModal.open();
+          });
+     });
+}
